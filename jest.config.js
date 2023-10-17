6689a1145fbe537980e618a43e1135fa27b6103d
Luis Barzola <lrbarzola@gmail.com>
2023-10-17 09:49:02 -0300

    Added readme

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  cacheDirectory: '.tmp/jestCache'
+  cacheDirectory: '.tmp/jestCache',
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "test-config",
+    "interfaces",
+    "jestGlobalMocks.ts",
+    ".module.ts",
+    "<rootDir>/src/app/main.ts",
+    ".mock.ts",
+    "dist",
+]
 };
